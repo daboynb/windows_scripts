8b0fb7c4b1ab8002a199a9aa1997913da39eb2ce
daboynb <106079917+daboynb@users.noreply.github.com>
2022-10-17 14:31:06 +0200

    up

---
+++ Remove Edge/remove_edge.ps1
@@ -0,0 +1,2 @@
+Write-Output "Removing Edge"
+Set-Location "C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer" ; .\setup.exe -uninstall -system-level -verbose-logging -force-uninstall 
--- Remove Onedrive/remove_onedrive.ps1
+++ Remove Onedrive/remove_onedrive.ps1
@@ -1,12 +1,12 @@
 if ((Get-WmiObject win32_operatingsystem | Select-Object osarchitecture).osarchitecture -eq "64 bit")
 {
     #64 bit logic here
-    Write-Output "Removing"
+    Write-Output "Removing OneDrive"
     %systemroot%\SysWOW64\OneDriveSetup.exe /uninstall
 }
 else
 {
     #32 bit logic here
-    Write-Output "Removing"
+    Write-Output "Removing OneDrive"
     %systemroot%\System32\OneDriveSetup.exe /uninstall
 }
