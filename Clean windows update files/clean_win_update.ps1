8b1ac6e892b0e8a4b9b1ed568681ab0cdbbb253c
daboynb <106079917+daboynb@users.noreply.github.com>
2022-11-08 09:03:30 +0100

    up

--- Clean windows update files/clean_win_update.ps1
+++ Clean windows update files/clean_win_update.ps1
@@ -3,3 +3,4 @@ net stop wuauserv | out-null
 Get-ChildItem -LiteralPath C:\Windows\SoftwareDistribution\Download -Recurse | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
 Write-Host "Starting Windows Update Service..." 
 net start wuauserv | out-null 
+Write-Output "Done"
