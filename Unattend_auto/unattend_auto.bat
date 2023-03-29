f082bed655a86d1649a64052a1e37b6554f8cb7d
daboynb <106079917+daboynb@users.noreply.github.com>
2023-03-29 20:53:20 +0200

    Update unattend_auto.bat

--- Unattend_auto/unattend_auto.bat
+++ Unattend_auto/unattend_auto.bat
@@ -4,7 +4,7 @@ rem Ask for admin privileges
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-rem create folders
+rem create folder
 mkdir "C:\ISO\Win11" 
 
 rem set iso path
