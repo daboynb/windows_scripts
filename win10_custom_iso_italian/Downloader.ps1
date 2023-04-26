9468bbd83632d345f96f314ce4687886fd48c72b
daboynb <106079917+daboynb@users.noreply.github.com>
2023-04-26 23:56:25 +0200

    Update Downloader.ps1

--- win10_custom_iso_italian/Downloader.ps1
+++ win10_custom_iso_italian/Downloader.ps1
@@ -1,20 +1,20 @@
-# Set the path for the desktop folder
-$desktopPath = [Environment]::GetFolderPath("Desktop")
+# Change to the desktop directory
+cd "$env:USERPROFILE\Desktop"
 
 # Download the script from GitHub
-Invoke-WebRequest -Uri "https://github.com/daboynb/windows_scripts/archive/refs/heads/main.zip" -OutFile "$desktopPath\windows_script_daboynb.zip"
+Invoke-WebRequest -Uri "https://github.com/daboynb/windows_scripts/archive/refs/heads/main.zip" -OutFile "windows_script_daboynb.zip"
 
-# Extract all from "windows_script_daboynb.zip" to the desktop
-Expand-Archive -Path "$desktopPath\windows_script_daboynb.zip" -DestinationPath "$desktopPath" -Force
+# Extract all from "windows_script_daboynb.zip"
+Expand-Archive -Path "windows_script_daboynb.zip" -DestinationPath "." -Force
 
-# Move the "win10_custom_iso_italian" folder to the desktop
-Move-Item -Path "$desktopPath\windows_scripts-main\win10_custom_iso_italian" -Destination "$desktopPath\win10_custom_iso_italian" -Force
+# Move the "win10_custom_iso_italian" folder to the current directory
+Move-Item -Path "windows_scripts-main\win10_custom_iso_italian" -Destination "win10_custom_iso_italian" -Force
 
-# Remove the "./windows_scripts-main/" directory
-Remove-Item -Path "$desktopPath\windows_scripts-main" -Recurse -Force
+# Remove the "windows_scripts-main" directory 
+Remove-Item -Path "windows_scripts-main" -Recurse -Force
 
-# Remove "windows_script_daboynb.zip" file
-Remove-Item -Path "$desktopPath\windows_script_daboynb.zip" -Force
+# Remove the "windows_script_daboynb.zip" file 
+Remove-Item -Path "windows_script_daboynb.zip" -Force
 
-# Execute the script
-Start-Process -FilePath "$desktopPath\win10_custom_iso_italian\win10_custom_iso_Italian.bat"
+# Run the script
+Start-Process -FilePath ".\win10_custom_iso_italian\win10_custom_iso_Italian.bat"
