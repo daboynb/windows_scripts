849ee3c96d49b5d20b6edf31f7642e9c5e14aa5d
daboynb <106079917+daboynb@users.noreply.github.com>
2023-04-26 23:51:03 +0200

    Update Downloader.ps1

--- win10_custom_iso_italian/Downloader.ps1
+++ win10_custom_iso_italian/Downloader.ps1
@@ -1,17 +1,20 @@
-# Download the from GitHub repository 
-Invoke-WebRequest -Uri "https://github.com/daboynb/windows_scripts/archive/refs/heads/main.zip" -OutFile "windows_script_daboynb.zip"
+# Set the path for the desktop folder
+$desktopPath = [Environment]::GetFolderPath("Desktop")
 
-# Extract all from "windows_script_daboynb.zip"
-Expand-Archive -Path "windows_script_daboynb.zip" -DestinationPath "." -Force
+# Download the script from GitHub
+Invoke-WebRequest -Uri "https://github.com/daboynb/windows_scripts/archive/refs/heads/main.zip" -OutFile "$desktopPath\windows_script_daboynb.zip"
 
-# Move the "win10_custom_iso_italian" folder to the current directory
-Move-Item -Path "./windows_scripts-main/win10_custom_iso_italian" -Destination "./win10_custom_iso_italian" -Force
+# Extract all from "windows_script_daboynb.zip" to the desktop
+Expand-Archive -Path "$desktopPath\windows_script_daboynb.zip" -DestinationPath "$desktopPath" -Force
 
-# Remove the "./windows_scripts-main/" directory 
-Remove-Item -Path "./windows_scripts-main" -Recurse -Force
+# Move the "win10_custom_iso_italian" folder to the desktop
+Move-Item -Path "$desktopPath\windows_scripts-main\win10_custom_iso_italian" -Destination "$desktopPath\win10_custom_iso_italian" -Force
 
-# Remove  "windows_script_daboynb.zip" file 
-Remove-Item -Path "./windows_script_daboynb.zip" -Force
+# Remove the "./windows_scripts-main/" directory
+Remove-Item -Path "$desktopPath\windows_scripts-main" -Recurse -Force
 
-# Run it
-win10_custom_iso_italian\win10_custom_iso_Italian.bat
+# Remove "windows_script_daboynb.zip" file
+Remove-Item -Path "$desktopPath\windows_script_daboynb.zip" -Force
+
+# Execute the script
+Start-Process -FilePath "$desktopPath\win10_custom_iso_italian\win10_custom_iso_Italian.bat"
