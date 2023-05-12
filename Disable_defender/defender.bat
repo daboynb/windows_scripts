7b819d7da1c3b203e886135bd2af3785f5a4b5c5
daboynb <106079917+daboynb@users.noreply.github.com>
2023-05-12 23:58:37 +0200

    up

---
+++ Disable_defender/defender.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+rem Ask for admin privileges
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul 2>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
+PowerRun.exe cmd.exe /c "sc config WinDefend start= disabled"
+PowerRun.exe cmd.exe /c "sc stop WinDefend"
