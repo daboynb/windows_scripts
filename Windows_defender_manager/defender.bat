04f24cf07fe23da455b21d58f8ef4142883ff414
daboynb <106079917+daboynb@users.noreply.github.com>
2023-06-07 19:32:04 +0200

    Update defender.bat

--- Windows_defender_manager/defender.bat
+++ Windows_defender_manager/defender.bat
@@ -64,7 +64,7 @@ PowerRun.exe cmd.exe /c "reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows D
 PowerRun.exe cmd.exe /c "reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Features /v TamperProtection /t REG_DWORD /d 0 /f">NUL
 
 rem Disable Windows Defender antispyware and smartscreen
-PowerRun.exe cmd.exe /c "cd C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23050.3-0 && ren MsMpEng.exe MsMpEng.exe.bak"
+PowerRun.exe cmd.exe /c "for /r "C:\ProgramData\Microsoft\Windows Defender\Platform" %I in (MsMpEng.exe) do @if exist "%I" ren "%I" MsMpEng.exe.bak"
 PowerRun.exe cmd.exe /c reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 PowerRun.exe cmd.exe /c "cd C:\Windows\System32 && ren smartscreen.exe smartscreen.exe.bak"
 start explorer.exe
@@ -114,7 +114,7 @@ PowerRun.exe cmd.exe /c "reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows D
 PowerRun.exe cmd.exe /c "reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Features /v MpCapability /t REG_BINARY /d ff01000000000000 /f">NUL
 
 rem Enable Windows Defender antispyware and smartscreen
-PowerRun.exe cmd.exe /c "cd C:\ProgramData\Microsoft\Windows Defender\Platform\4.18.23050.3-0 && ren MsMpEng.exe.bak MsMpEng.exe"
+PowerRun.exe cmd.exe /c "for /r "C:\ProgramData\Microsoft\Windows Defender\Platform" %I in (MsMpEng.exe.bak) do @if exist "%I" ren "%I" MsMpEng.exe"
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 0 /f
 PowerRun.exe cmd.exe /c "cd C:\Windows\System32 && ren smartscreen.exe.bak smartscreen.exe"
 start explorer.exe
