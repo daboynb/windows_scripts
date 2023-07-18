20f53cd96a1c5021d605cf045e318db7e3543c74
daboynb <106079917+daboynb@users.noreply.github.com>
2023-07-18 21:13:21 +0200

    up

--- windows_11_section_manager/section_manager.bat
+++ windows_11_section_manager/section_manager.bat
@@ -7,6 +7,18 @@ rem Ask for admin privileges
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
+IF exist dxgi.dll (
+    echo ok
+) ELSE (
+    echo dxgi.dll not present!
+)
+
+IF exist files_detect.ps1 (
+    echo ok
+) ELSE (
+    echo files_detect.ps1 not present!
+)
+
 :MAINMENU
 CLS
 SET MENU=
