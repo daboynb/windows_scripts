aa05f8889f0665e49213c1a14617c7288b5600c6
daboynb <106079917+daboynb@users.noreply.github.com>
2023-07-27 13:27:11 +0200

    Update add_store_windows_10_ltsc_2021.bat

--- Add store windows ltsc 2021/add_store_windows_10_ltsc_2021.bat
+++ Add store windows ltsc 2021/add_store_windows_10_ltsc_2021.bat
@@ -5,7 +5,7 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
 echo Installing the store... Please wait.
-WSReset -i >NUL
+WSReset -i 1>nul 2>nul
 TimeOut 20 
-WSReset -i >NUL
+WSReset -i 1>nul 2>nul
 echo Store is downloading... it will be installed soon
