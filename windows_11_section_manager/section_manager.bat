165a0904ff7917698fa8b97a5859b1877dee141e
daboynb <106079917+daboynb@users.noreply.github.com>
2023-08-29 12:36:03 +0200

    update

--- windows_11_section_manager/section_manager.bat
+++ windows_11_section_manager/section_manager.bat
@@ -7,17 +7,14 @@ rem Ask for admin privileges
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-IF exist dxgi.dll (
-    echo ok
-) ELSE (
-    echo dxgi.dll not present! && pause && exit /b 1
-)
-
 IF exist files_detect.ps1 (
     echo ok
 ) ELSE (
     echo files_detect.ps1 not present! && pause && exit /b 1
 )
+
+ping 8.8.8.8 -n 1 -w 1000 > nul
+IF "%ERRORLEVEL%"=="1" color 0C && echo "Internet is not avibale, EXITING!" && pause && exit
 
 :MAINMENU
 CLS
@@ -37,9 +34,21 @@ GOTO :MAINMENU
 rem ####################################################################################### INSTALL SECTION
 
 :INSTALL
+powershell -Command "$url = (Invoke-RestMethod -Uri 'https://api.github.com/repos/valinet/ExplorerPatcher/releases/latest').assets[0].browser_download_url; Invoke-WebRequest -Uri $url -OutFile \"$env:USERPROFILE\\Desktop\\ep_setup.exe\""
+
+"%userprofile%\Desktop\ep_setup.exe" /extract "%userprofile%\Desktop\ep_setup"
+copy "%userprofile%\Desktop\ep_setup\ExplorerPatcher.amd64.dll" "%userprofile%\Desktop\dxgi.dll"
+rmdir "%userprofile%\Desktop\ep_setup" /s /q
+del "%userprofile%\Desktop\ep_setup.exe"
+IF exist "%userprofile%\Desktop\dxgi.dll" (
+    echo ok
+) ELSE (
+    echo dxgi.dll not present! && pause && exit /b 1
+)
+
 rem copy dll's
-copy "dxgi.dll" "%SystemRoot%\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\dxgi.dll"
-copy "dxgi.dll" "%SystemRoot%\dxgi.dll"
+copy "%userprofile%\Desktop\dxgi.dll" "%SystemRoot%\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\dxgi.dll"
+copy "%userprofile%\Desktop\dxgi.dll" "%SystemRoot%\dxgi.dll"
 
 rem add keys
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\ExplorerPatcher" /v "StartDocked_DisableRecommendedSection" /t REG_DWORD /d 1 /f
