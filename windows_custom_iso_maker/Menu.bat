6273974140a469f8abb23cf18cacb2d547015996
daboynb <106079917+daboynb@users.noreply.github.com>
2023-09-22 16:45:01 +0200

    Update Menu.bat

--- windows_custom_iso_maker/Menu.bat
+++ windows_custom_iso_maker/Menu.bat
@@ -11,6 +11,28 @@ echo *******************************
 echo *   WINDOWS CUSTOM ISO MAKER  *
 echo *******************************
 echo.
+echo [1] WINDOWS 11 
+echo.
+echo [2] WINDOWS 10
+echo.
+echo [0] EXIT
+echo.
+echo *******************************
+SET /P MENU=Type 1, 2 or 0 then press ENTER : 
+IF /I '%MENU%'=='1' GOTO :submenu_11
+IF /I '%MENU%'=='2' GOTO :submenu_10
+IF /I '%MENU%'=='0' GOTO :EXIT
+GOTO :MAINMENU
+
+:submenu_11
+CLS
+color 0A
+SET MENU=
+cls
+echo *******************************
+echo *   WINDOWS CUSTOM ISO MAKER  *
+echo *******************************
+echo.
 echo [1] WINDOWS 11 UEFI ITALIAN
 echo    (Crea un'ISO di Windows 11 debloated in modalita' UEFI. Non include il bypass della RAM minima di 4 GB,
 echo     tutti gli altri requisiti sono bypassati: CPU, TPM e SECUREBOOT)
@@ -27,24 +49,40 @@ echo [4] WINDOWS 11 LEGACY ENGLISH
 echo    (Create a debloated Windows 11 ISO in legacy mode.
 echo     All the requisites are bypassed, including the 4 GB minimum RAM bypass)
 echo.
-echo [5] WINDOWS 10 ITA
+echo [0] EXIT
+echo.
+echo *******************************
+SET /P MENU=Type 1, 2, 3, 4, or 0 then press ENTER : 
+IF /I '%MENU%'=='1' GOTO :win_11_uefi_ita
+IF /I '%MENU%'=='2' GOTO :win_11_uefi_en
+IF /I '%MENU%'=='3' GOTO :win_11_legacy_ita
+IF /I '%MENU%'=='4' GOTO :win_11_legacy_en
+IF /I '%MENU%'=='0' GOTO :EXIT
+GOTO :submenu_11
+
+:submenu_10
+CLS
+color 0A
+SET MENU=
+cls
+echo *******************************
+echo *   WINDOWS CUSTOM ISO MAKER  *
+echo *******************************
+echo.
+echo [1] WINDOWS 10 ITA
 echo    (Crea una ISO debloated si Windows 10)
 echo.
-echo [6] WINDOWS 10 EN
+echo [2] WINDOWS 10 EN
 echo    (Create a debloated Windows 10 ISO)
 echo.
 echo [0] EXIT
 echo.
 echo *******************************
-SET /P MENU=Type 1, 2, 3, 4, 5 ,6 or 0 then press ENTER : 
-IF /I '%MENU%'=='1' GOTO :win_11_uefi_ita
-IF /I '%MENU%'=='2' GOTO :win_11_uefi_en
-IF /I '%MENU%'=='3' GOTO :win_11_legacy_ita
-IF /I '%MENU%'=='4' GOTO :win_11_legacy_en
-IF /I '%MENU%'=='5' GOTO :win_10_it
-IF /I '%MENU%'=='6' GOTO :win_10_en
+SET /P MENU=Type 1, 2, or 0 then press ENTER : 
+IF /I '%MENU%'=='1' GOTO :win_10_it
+IF /I '%MENU%'=='2' GOTO :win_10_en
 IF /I '%MENU%'=='0' GOTO :EXIT
-GOTO :MAINMENU
+GOTO :submenu_10
 
 :win_11_uefi_ita
 win11_custom_iso_italian_uefi\Runme.bat
