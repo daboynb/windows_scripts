b8a9d62131dcbe2800545a94beec8f4a2a02e179
daboynb <106079917+daboynb@users.noreply.github.com>
2023-11-04 16:20:49 +0100

    fix

--- windows_custom_iso_maker/win11_custom_iso_en/create.bat
+++ windows_custom_iso_maker/win11_custom_iso_en/create.bat
@@ -84,12 +84,6 @@ IF NOT EXIST "C:\ISO\Win11\sources\$OEM$\$$\Panther" (
     mkdir "C:\ISO\Win11\sources\$OEM$\$$\Panther"
 )
 
-rem edit unattend.xml
-powershell -command "Write-Host 'Setting up locale...' -ForegroundColor Green; $output = dism /image:C:\mount\mount /get-intl | Select-String -Pattern 'Default system UI language : (\w{2}-\w{2})' | Foreach-Object { $_.Matches.Groups[1].Value }; (Get-Content -path resources\unattend_edited.xml -Raw) -replace 'locale_set',$output | Set-Content -Path resources\unattend_edited.xml"
-del resources\unattend.xml
-ren resources\unattend_edited.xml resources\unattend.xml
-cls
-
 rem copy unattended.xml
 copy "resources\unattend.xml" "C:\ISO\Win11\sources\$OEM$\$$\Panther\unattend.xml"
 IF %errorlevel% equ 0 (
