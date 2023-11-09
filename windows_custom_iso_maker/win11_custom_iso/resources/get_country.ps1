3cc2e8ca604eb02fe9f7f4a1839d43f112b5fb04
daboynb <106079917+daboynb@users.noreply.github.com>
2023-11-09 18:49:58 +0100

    new stuff

--- windows_custom_iso_maker/win11_custom_iso/resources/get_country.ps1
+++
@@ -1,5 +0,0 @@
-# Run the PowerShell command and save the output to a variable
-$myvar = (curl.exe -s ipinfo.io | Select-String -Pattern '\"country\": \"(.*?)\"').Matches.Groups[1].Value
-
-# Output the value of $myvar
-$myvar
