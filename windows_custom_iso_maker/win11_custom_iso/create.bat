942983de1e3c399fc2d125263c265b23f30f9c1c
daboynb <106079917+daboynb@users.noreply.github.com>
2024-03-25 19:04:27 +0100

    gui for windows custom iso maker

--- windows_custom_iso_maker/win11_custom_iso/create.bat
+++ windows_custom_iso_maker/win11_custom_iso/create.bat
@@ -1,36 +1,36 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-cls
+rem ############################################################################## arguments and vars
+rem Extracting arguments
+set "selectedFile=%~1"
+set "windowsVersion=%~2"
+set "edgeRemovalPreference=%~3"
+set "defenderPreference=%~4"
+set "windowsEdition=%~5"
 
-rem Ask for admin privileges
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+for %%I in ("%selectedFile%") do set "dest_path=%%~dpI"
 
-rem ##########################
-rem reload cmd for quickeditmode , thanks to https://stackoverflow.com/a/21805713
-if exist "%TEMP%\consoleSettingsBackup.reg" regedit /S "%TEMP%\consoleSettingsBackup.reg"&DEL /F /Q "%TEMP%\consoleSettingsBackup.reg"&goto :mainstart
-regedit /S /e "%TEMP%\consoleSettingsBackup.reg" "HKEY_CURRENT_USER\Console"
-echo REGEDIT4>"%TEMP%\disablequickedit.reg"
-echo [HKEY_CURRENT_USER\Console]>>"%TEMP%\disablequickedit.reg"
-(echo "QuickEdit"=dword:00000000)>>"%TEMP%\disablequickedit.reg"
-regedit /S "%TEMP%\disablequickedit.reg"
-DEL /F /Q "%TEMP%\disablequickedit.reg"
-start "" "cmd" /c "%~dpnx0"&exit
+rem export windows edition
+if "%windowsEdition%"=="Home" (
+    set "index=1"
+) else (
+    set "index=5"
+)
+echo %index%
 
-:mainstart
-rem ##########################
-
-powerShell -Command "Write-Host 'My github -> https://github.com/daboynb' -ForegroundColor Green; exit" && timeout 04>nul
+set "path_to_use=C:\"
+rem ##############################################################################
 
 title win11_custom_iso
 
+rem ############################################################################## check files and create folders
 rem check if the resources folder exist
-IF NOT EXIST "resources" (
+IF NOT EXIST "%path_to_use%\windows_custom_iso_maker\win10_custom_iso\resources" (
     color 4 && echo "ERROR: Can't find the resources folder" && pause && exit /b 1
 )
 
-set "resource_dir=resources"
+set "resource_dir=%path_to_use%\windows_custom_iso_maker\win11_custom_iso\resources"
 set "files=7z.dll 7z.exe firefox_installer.exe oscdimg.exe tweaks.bat unattend.xml start.ps1 PowerRun.exe get_country.ps1"
 
 for %%i in (%files%) do (
@@ -65,78 +65,22 @@ IF %errorlevel% equ 0 (
   color 4 && echo "ERROR: Can't create C:\mount\mount!" && pause && exit /b 1
 )
 
-powerShell -Command "Write-Host 'You need the stock win11 ISO to continue' -ForegroundColor Green; exit" && timeout 04 >nul 
-
-:loop
-ping 8.8.8.8 -n 1 >nul
-if %errorlevel% equ 0 (
-    goto :end
-) else (
-    echo Internet connection not available. Retrying...
-    timeout /t 5 >nul
-    cls
-    goto :loop
-)
-:end
-
-rem Call the PowerShell script and capture its output to a variable
-for /f %%i in ('powershell -executionpolicy bypass -file resources\get_country.ps1') do (
-    set countryvar=%%i
-)
-
-rem ISO if IT
-IF "%countryvar%" == "IT" (
-  start "" "https://tinyurl.com/4ty38tpk"
-)
-
-powerShell -Command "Write-Host 'Press enter when you have downloaded it' -ForegroundColor Green; exit" && timeout 04 >nul 
-pause
-cls
-
-rem set iso path
-set "filepath="
-set "dialogTitle=Select a file"
-
-rem Open file dialog to select file
-:select_file
-powerShell -Command "Write-Host 'Select win11 ISO file' -ForegroundColor Green; exit"  
-for /f "usebackq delims=" %%f in (`powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog; $openFileDialog.InitialDirectory = [Environment]::GetFolderPath('Desktop'); $openFileDialog.Title = '%dialogTitle%'; $openFileDialog.Filter = 'ISO files (*.iso)|*.iso'; $openFileDialog.FilterIndex = 1; $openFileDialog.Multiselect = $false; $openFileDialog.ShowDialog() | Out-Null; $openFileDialog.FileName}"`) do set "filepath=%%f"
-
-if defined filepath (
-  powerShell -Command "Write-Host 'You selected %filepath%' -ForegroundColor Green; exit"  
-  cls
-) ELSE (
-  echo No file selected
-  goto :select_file
-)
-
 powerShell -Command "Write-Host 'Extracting ISO to C:\ISO\Win11... Please wait!' -ForegroundColor Green; exit"  
-resources\7z.exe x -y -o"C:\ISO\Win11" "%filepath%" > nul
+%resource_dir%\7z.exe x -y -o"C:\ISO\Win11" "%selectedFile%" > nul
 IF %errorlevel% equ 0 (
   cls
 ) ELSE (
   color 4 && echo "ERROR: Extraction failed!" && pause && rmdir "C:\mount" /s /q && rmdir "C:\ISO" /s /q && exit /b 1
 )
-
-rem Open folder dialog to select the folder where the ISO will be saved
-:select_folder
-powerShell -Command "Write-Host 'Select the folder where the iso will be saved' -ForegroundColor Green; exit"  
-for /f "usebackq delims=" %%f in (`powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; $folderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog; $folderBrowserDialog.RootFolder = 'Desktop'; $folderBrowserDialog.Description = '%dialogTitle%'; $folderBrowserDialog.ShowNewFolderButton = $false; $result = $folderBrowserDialog.ShowDialog(); if ($result -eq 'OK') { Write-Output $folderBrowserDialog.SelectedPath; } else { Write-Output ''; } }"`) do set "path_to_use=%%f"
-
-if defined path_to_use (
-  powerShell -Command "Write-Host 'You selected %path_to_use%' -ForegroundColor Green; exit"  
-  cls
-) ELSE (
-  echo No folder selected
-  goto :select_folder
-)
-
+rem ##############################################################################
+rem ############################################################################## export wim and set unattend
+rem set iso path
 IF NOT EXIST "C:\ISO\Win11\sources\$OEM$\$$\Panther" (
     mkdir "C:\ISO\Win11\sources\$OEM$\$$\Panther"
 )
 
 rem copy unattended.xml
-copy "resources\unattend.xml" "C:\ISO\Win11\sources\$OEM$\$$\Panther\unattend.xml"
+copy "%resource_dir%\unattend.xml" "C:\ISO\Win11\sources\$OEM$\$$\Panther\unattend.xml"
 IF %errorlevel% equ 0 (
   cls
 ) ELSE (
@@ -153,38 +97,11 @@ IF EXIST "C:\ISO\Win11\sources\install.esd" (
 )
 
 :esd
-dism /English /Get-WimInfo /WimFile:"C:\ISO\Win11\sources\install.esd"
-echo.
-powerShell -Command "Write-Host 'Select the windows version you want to use' -ForegroundColor Green; exit"
-echo.
-set /p index="Please enter the number of the index: "
-cls
 powerShell -Command "Write-Host 'Exporting' -ForegroundColor Green; exit"
 dism /English /export-image /SourceImageFile:"C:\ISO\Win11\sources\install.esd" /SourceIndex:%index% /DestinationImageFile:"C:\ISO\Win11\sources\install.wim" /Compress:max /CheckIntegrity
 goto :copy_esd
 
 :wim
-rem export windows edition
-
-echo.
-powerShell -Command "Write-Host 'Select the windows version you want to use' -ForegroundColor Green; exit"
-echo.
-echo.===============================================================================
-echo.^| Index ^| Arch ^| Name
-echo.===============================================================================
-for /f "tokens=2 delims=: " %%a in ('dism /English /Get-WimInfo /WimFile:"C:\ISO\Win11\sources\install.wim" ^| findstr Index') do (
-    for /f "tokens=2 delims=: " %%b in ('dism /English /Get-WimInfo /WimFile:"C:\ISO\Win11\sources\install.wim" /Index:%%a ^| findstr /i Architecture') do (
-        for /f "tokens=* delims=:" %%c in ('dism /English /Get-WimInfo /WimFile:"C:\ISO\Win11\sources\install.wim" /Index:%%a ^| findstr /i Name') do (
-            set "Name=%%c"
-            if %%a equ 1 echo.^|  %%a  ^| %%b ^| !Name!
-            if %%a gtr 1 if %%a leq 9 echo.^|  %%a  ^| %%b ^| !Name!
-            if %%a gtr 9 echo.^|  %%a ^| %%b ^| !Name!
-        )
-    )
-)
-
-set /p index="Enter an index number: "
-
 dism /English /Export-Image /SourceImageFile:"C:\ISO\Win11\sources\install.wim" /SourceIndex:%index% /DestinationImageFile:"C:\ISO\Win11\sources\install_pro.wim" /compress:max
 
 IF %errorlevel% equ 0 (
@@ -218,9 +135,8 @@ IF %errorlevel% equ 0 (
 ) ELSE (
   color 4 && echo "ERRORE: Can''t delete old install.esd!" && pause && rmdir "C:\mount" /s /q && rmdir "C:\ISO" /s /q && exit /b 1
 )
-
-rem ######################################################################################## 
-
+rem ##############################################################################
+rem ############################################################################## mount the image and customize
 :mountstep
 rem mount the image with dism /English
 powerShell -Command "Write-Host 'Mounting image' -ForegroundColor Green; exit"  
@@ -228,35 +144,27 @@ dism /English /mount-image /imagefile:"C:\ISO\Win11\sources\install.wim" /index:
 cls
 
 rem disable defender
-:defender
-set /p answer="Do you want to disable Windows Defender (Antivirus)? (yes/no) : "
-if /i "%answer%"=="yes" (
+:edge
+if "%defenderPreference%"=="Disable Windows Defender" (
     echo > C:\mount\mount\Windows\nodefender.pref
-    set defender_status=defender_disabled
-) else if /i "%answer%"=="no" (
-    set defender_status=defender_enabled
-) ELSE (
-    echo Invalid input. Please answer with 'yes' or 'no'.
-    goto :defender
+    set defender_status=whithout_defender
+) else (
+    set defender_status=with_defender
 )
 
 rem delete edge
 :edge
-set /p answer="Do you want to remove Edge? (yes/no): "
-if /i "%answer%"=="yes" (
+if "%edgeRemovalPreference%"=="Remove Edge" (
     echo > C:\mount\mount\Windows\noedge.pref
-    set edge_status=edge_removed
+    set edge_status=without_edge
     goto :edge_step
-) else if /i "%answer%"=="no" (
-    set edge_status=edge_not_removed
+) else (
+    set edge_status=with_edge
     goto :features
-) ELSE (
-    echo Invalid input. Please answer with 'yes' or 'no'.
-    goto :edge
 )
 
 :edge_step
-copy "resources\firefox_installer.exe" "C:\mount\mount"
+copy "%resource_dir%\firefox_installer.exe" "C:\mount\mount"
 IF %errorlevel% equ 0 (
   cls
 ) ELSE (
@@ -279,11 +187,16 @@ powerShell -Command "Write-Host 'Done' -ForegroundColor Green; exit"
 rem copy batch file
 cls
 powerShell -Command "Write-Host 'Copying bat' -ForegroundColor Green; exit"
-copy "resources\tweaks.bat" "C:\mount\mount\Windows"
+copy "%resource_dir%\tweaks.bat" "C:\mount\mount\Windows"
 IF %errorlevel% equ 0 (
   cls
 ) ELSE (
   color 4 && echo "Can't copy tweaks.bat!" && pause && rmdir "C:\mount" /s /q && rmdir "C:\ISO" /s /q && exit /b 1
+)
+
+rem Call the PowerShell script and capture its output to a variable
+for /f %%i in ('powershell -executionpolicy bypass -file resources\get_country.ps1') do (
+    set countryvar=%%i
 )
 
 rem copy debloat if IT
@@ -297,7 +210,7 @@ IF "%countryvar%" == "IT" (
 )
 
 rem copy start.ps1
-copy "resources\start.ps1" "C:\mount\mount\Windows"
+copy "%resource_dir%\start.ps1" "C:\mount\mount\Windows"
 IF %errorlevel% equ 0 (
   cls
 ) ELSE (
@@ -305,7 +218,7 @@ IF %errorlevel% equ 0 (
 )
 
 rem copy PowerRun.exe
-copy "resources\PowerRun.exe" "C:\mount\mount\Windows"
+copy "%resource_dir%\PowerRun.exe" "C:\mount\mount\Windows"
 IF %errorlevel% equ 0 (
   cls
 ) ELSE (
@@ -317,10 +230,10 @@ rem unmount the image
 powerShell -Command "Write-Host 'Unmounting image' -ForegroundColor Green; exit"  
 dism /English /unmount-image /mountdir:"C:\mount\mount" /commit
 cls
-
-rem ######## boot.wim edits #################
+rem ##############################################################################
+rem ############################################################################## boot.wim edits 
 rem mount the image with dism /English
-powerShell -Command "Write-Host 'Sto montando l''immagine' -ForegroundColor Green; exit"  
+powerShell -Command "Write-Host 'Mounting the image' -ForegroundColor Green; exit"  
 dism /English /mount-image /imagefile:"C:\ISO\Win11\sources\boot.wim" /index:2 /mountdir:"C:\mount\mount"
 cls
 
@@ -336,14 +249,14 @@ cls
 timeout 04
 
 rem unmount the image
-powerShell -Command "Write-Host 'Smontando l''immagine' -ForegroundColor Green; exit"  
+powerShell -Command "Write-Host 'Unmounting the image' -ForegroundColor Green; exit"  
 dism /English /unmount-image /mountdir:"C:\mount\mount" /commit
 cls
-rem ######## boot.wim edits #################
-
+rem ##############################################################################
+rem ############################################################################## Build the iso
 rem rebuild image 
 powerShell -Command "Write-Host 'Building the ISO' -ForegroundColor Green; exit"  
-resources\oscdimg -m -o -u2 -bootdata:2#p0,e,bC:\ISO\Win11\boot\etfsboot.com#pEF,e,bC:\ISO\Win11\efi\microsoft\boot\efisys.bin C:\ISO\Win11 "%path_to_use%\Windows11_%defender_status%_%edge_status%.iso"
+%resource_dir%\oscdimg -m -o -u2 -bootdata:2#p0,e,bC:\ISO\Win11\boot\etfsboot.com#pEF,e,bC:\ISO\Win11\efi\microsoft\boot\efisys.bin C:\ISO\Win11 "%dest_path%\Windows11_%defender_status%_%edge_status%.iso"
 IF %errorlevel% equ 0 (
   cls
 ) ELSE (
@@ -363,15 +276,16 @@ IF %errorlevel% equ 0 (
 ) ELSE (
   color 4 && echo "ERROR: Can't delete the working folder2!" && pause && rmdir "C:\mount" /s /q && rmdir "C:\ISO" /s /q && exit /b 1
 )
-
-:: Enable QuickEdit Mode
+rem ##############################################################################
+rem ############################################################################## Clean 
+rem  Enable QuickEdit Mode
 reg add HKCU\Console /v QuickEdit /t REG_DWORD /d 1 /f
-
+echo. 
 powerShell -Command "Write-Host 'Process completed!' -ForegroundColor Green; exit"  
-
-rem flash iso
-powerShell -Command "Write-Host 'On the ISO''s path you will find a forked rufus too that works with custom ISOs' -ForegroundColor Green; exit"  
-copy "resources\rufus_forked_for_custom.exe" %path_to_use%
-powerShell -Command "Write-Host 'Completed, press enter to close the script!' -ForegroundColor Green; exit"  
+echo "The edited iso is here %dest_path%"
+echo. 
+copy "%resource_dir%\rufus_forked_for_custom.exe" %dest_path% > NUL
+echo "The normal rufus is not compatible with this ISO, use the rufus_forked_for_custom.exe in %dest_path%"
 pause
 endlocal
+rem ##############################################################################
