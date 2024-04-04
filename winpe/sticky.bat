0b1cf364afca524f870f7e1017ad7e3855611727
daboynb <106079917+daboynb@users.noreply.github.com>
2024-04-05 00:10:46 +0200

    Update sticky.bat

--- winpe/sticky.bat
+++ winpe/sticky.bat
@@ -7,9 +7,8 @@ ECHO WINDOWS BYPASS
 echo.
 ECHO [1] Add an administrator account
 ECHO [2] Change user password
-ECHO [0] EXIT
 echo.
-SET /P MENU=Type 1, 2 or 0 then press ENTER :
+SET /P MENU=Type 1, 2 then press ENTER :
 IF /I '%MENU%'=='1' GOTO :ADMIN
 IF /I '%MENU%'=='2' GOTO :CHANGE
 GOTO :MAINMENU
