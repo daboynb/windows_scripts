e18cc27c80493cd00eb12b49cef268ad51d814bc
daboynb <106079917+daboynb@users.noreply.github.com>
2024-04-17 09:08:02 +0200

    Update start.ps1

--- windows_custom_iso_maker/win11_custom_iso/resources/start.ps1
+++ windows_custom_iso_maker/win11_custom_iso/resources/start.ps1
@@ -60,47 +60,6 @@ Disable-UserInput
 
 Start-Sleep -Seconds 30 | Out-Null
 
-######################################################################### Allow edge unistall
-# That's the JSON where the configs are stored
-$integratedServicesPath = "C:\Windows\System32\IntegratedServicesRegionPolicySet.json"
-
-if (Test-Path $integratedServicesPath) {
-
-    # Get the permissions (ACL) of the original file
-    $acl = Get-Acl -Path $integratedServicesPath
-
-    # Take ownership of the file
-    takeown /f $integratedServicesPath /a 
-
-    # Grant the full control to be able to edit it
-    icacls $integratedServicesPath /grant Administrators:F
-
-    # Read the JSON
-    $jsonContent = Get-Content $integratedServicesPath | ConvertFrom-Json
-
-    # Edit the JSON to allow uninstall
-    foreach ($policy in $jsonContent.policies) {
-        if ($policy.'$comment' -like "*Edge*" -and $policy.'$comment' -like "*uninstall*") {
-            $policy.defaultState = 'enabled'
-            # Set region to all ISO 3166-1 alpha-2 country codes
-            $allCountryCodes = @("AT", "BE", "BG", "CH", "CY", "CZ", "DE", "DK", "EE", "ES", "FI", "FR", "GF", "GP", "GR", "HR", "HU", "IE", "IS", "IT", "LI", "LT", "LU", "LV", "MT", "MQ", "NL", "NO", "PL", "PT", "RE", "RO", "SE", "SI", "SK", "YT", "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR", "AS", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BF", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CC", "CD", "CF", "CG", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CX", "DJ", "DM", "DO", "DZ", "EC", "EG", "EH", "ER", "ET", "FK", "FM", "FO", "GA", "GB", "GD", "GE", "GG", "GH", "GI", "GL", "GM", "GN", "GQ", "GS", "GT", "GU", "GW", "GY", "HK", "HM", "HN", "HT", "ID", "IL", "IM", "IN", "IO", "IQ", "IR", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LK", "LR", "LS", "LY", "MA", "MC", "MD", "ME", "MF", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MR", "MS", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF", "NG", "NI", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PM", "PN", "PR", "PS", "PW", "PY", "QA", "RE", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SG", "SH", "SJ", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SY", "SZ", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TT", "TV", "TW", "TZ", "UA", "UG", "UM", "US", "UY", "UZ", "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE", "YT", "ZA", "ZM", "ZW")
-            $policy.conditions.region.enabled = $allCountryCodes
-        }
-    }
-
-    # Write the JSON file to another location to avoid the 'permission denied' error
-    $jsonContent | ConvertTo-Json -Depth 100 | Set-Content -Path "C:\BK_IntegratedServicesRegionPolicySet.json"
-
-    # Move the new JSON file to C:\Windows\System32\IntegratedServicesRegionPolicySet.json
-    copy C:\BK_IntegratedServicesRegionPolicySet.json C:\Windows\System32\IntegratedServicesRegionPolicySet.json
-
-    # Set the original permissions to the new file
-    Set-Acl -Path $integratedServicesPath -AclObject $acl
-}
-else {
-    # File does not exist
-    Write-Host "The file $integratedServicesPath does not exist."
-}
 #########################################################################
 
 # Start bat
