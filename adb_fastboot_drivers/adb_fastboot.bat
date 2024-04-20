d20ec6bd87ed40ca0da042c72f32a20485fc8e06
daboynb <106079917+daboynb@users.noreply.github.com>
2024-04-20 14:22:29 +0200

    Update adb_fastboot.bat

--- adb_fastboot_drivers/adb_fastboot.bat
+++ adb_fastboot_drivers/adb_fastboot.bat
@@ -6,69 +6,13 @@ cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) &&
 
 powerShell -Command "Write-Host 'My github -> https://github.com/daboynb' -ForegroundColor Green; exit" && timeout 04>nul
 
-chcp 65001>nul 2>&1
-echo.
-echo.
-echo ╔════════════════════════════════════════════════════════════════════════╗
-echo ║     Downloading latest platform-tools.zip...                           ║
-echo ╚════════════════════════════════════════════════════════════════════════╝
-echo.
-echo.
-
-rem Download the script from GitHub
+rem Download platform-tools-latest-windows.zip
 powershell -Command "Set-Variable ProgressPreference SilentlyContinue; Invoke-WebRequest "https://dl.google.com/android/repository/platform-tools-latest-windows.zip" -OutFile "C:\Program` Files\platform-tools.zip" | Out-Null"
-IF EXIST "C:\Program Files\platform-tools.zip" (
-  powerShell -Command "Write-Host 'Platform tools downloaded successfully' -ForegroundColor Green; exit" && timeout 04 >nul
-) ELSE (
-  color 4 && echo "ERROR, can''t download platform tools!" && pause && exit /b 1
-)
 
-echo.
-echo.
-echo ╔════════════════════════════════════════════════════════════════════════╗
-echo ║     Extracting latest platform-tools.zip...                            ║
-echo ╚════════════════════════════════════════════════════════════════════════╝
-echo.
-echo.
-
-rem Extract all from "windows_script_daboynb.zip"
+rem Extract all from platform-tools-latest-windows.zip
 powershell -Command "Expand-Archive -Path "C:\Program` Files\platform-tools.zip" -DestinationPath "C:\Program` Files\." -Force | Out-Null"
-IF EXIST "C:\Program Files\platform-tools" (
-  powerShell -Command "Write-Host 'Platform tools extracted successfully' -ForegroundColor Green; exit" && timeout 04 >nul
-) ELSE (
-  color 4 && echo "ERROR, can''t extract platform tools!" && pause && exit /b 1
-)
-
-echo.
-echo.
-echo ╔════════════════════════════════════════════════════════════════════════╗
-echo ║     Removing downloaded zip...                                         ║
-echo ╚════════════════════════════════════════════════════════════════════════╝
-echo.
-echo.
 
 rem Remove the C:\Program Files\platform-tools.zip file 
 powershell -Command "Remove-Item -Path "C:\Program` Files\platform-tools.zip" -Force | Out-Null"
-IF NOT EXIST "C:\Program Files\platform-tools.zip" (
-  powerShell -Command "Write-Host 'platform-tools.zip removed successfully' -ForegroundColor Green; exit" && timeout 04 >nul
-) ELSE (
-  color 4 && echo "ERROR, can''tremove platform-tools.zip!" && pause && exit /b 1
-)
-
-echo.
-echo.
-echo ╔════════════════════════════════════════════════════════════════════════╗
-echo ║     Setting path...                                                    ║
-echo ╚════════════════════════════════════════════════════════════════════════╝
-echo.
-echo.
 
 setx PATH "%PATH%;C:\Program Files\platform-tools"
-
-IF %errorlevel% equ 0 (
-  powerShell -Command "Write-Host 'Platform tools were added successfully to the system path' -ForegroundColor Green; exit" && timeout 04 >nul
-) ELSE (
-  color 4 && echo "ERROR, platform tools weren''t added successfully to the system path!" && pause && exit /b 1
-)
-
-pause
