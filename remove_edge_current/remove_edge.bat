c3a15f5a763e74d8a7b90f46667b42bcf36edc57
daboynb <106079917+daboynb@users.noreply.github.com>
2025-11-13 20:29:24 +0100

    update

--- remove_edge_current/remove_edge.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-rem Ask for admin privileges
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive%  1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-
-powershell -ExecutionPolicy Bypass -File remove_edge.ps1
